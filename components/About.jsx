4575487cbdfa3756d28446c30e24ce6ce4293da4
Nkematu Bonaventure <nkematu5@gmail.com>
2022-02-08 15:55:52 +0100

    made some major css updates to the about section

--- components/About.jsx
+++ components/About.jsx
@@ -4,22 +4,7 @@ import Heading from "./Heading";
 const About = () => {
   return (
     <section className={styles.about}>
-      <Heading text={"Your No. 1 website design and development agency"} />
       <div className={styles.container}>
-        <div>
-          <h3>
-            Your website should be the number one powerhouse for your business,
-            having an amazing combination of professionalism, aesthic appearance
-            as well as compelling enough to convince your audience on how much
-            they need your services.
-          </h3>
-          <p>
-            That's why at Clever Developers, we work hard at providing you with
-            a website that is tailored to suit your business goals, needs, as
-            well as objectives. We pride our services on its affordability
-            without any compromises on the quality of delivery.
-          </p>
-        </div>
         <div className={styles.about_us}>
           <Heading text={"About Us"} />
           <h3>
@@ -33,6 +18,21 @@ const About = () => {
             professional team of experts determined to give your brand a
             deserved online presence through the creation of bespoke websites as
             well as SEO services.
+          </p>
+        </div>
+        <div className={styles.copy}>
+          <Heading text={"Your No. 1 website design and development agency"} />
+          <h3>
+            Your website should be the number one powerhouse for your business,
+            having an amazing combination of professionalism, aesthic appearance
+            as well as compelling enough to convince your audience on how much
+            they need your services.
+          </h3>
+          <p>
+            That's why at Clever Developers, we work hard at providing you with
+            a website that is tailored to suit your business goals, needs, as
+            well as objectives. We pride our services on its affordability
+            without any compromises on the quality of delivery.
           </p>
         </div>
       </div>
