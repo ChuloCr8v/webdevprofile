eed43dcf1e19a2bf1a203fa31720a25a1515bac4
Nkematu Bonaventure <nkematu5@gmail.com>
2022-02-09 18:32:45 +0100

    created a responsive menu bar

--- pages/layout/Header.jsx
+++ pages/layout/Header.jsx
@@ -1,25 +1,35 @@
-import styles from '../../styles/Header.module.scss'
-import {FaBars} from 'react-icons/fa'
-import Menu from '../../components/Menu'
-import {useState} from 'react'
+import styles from "../../styles/Header.module.scss";
+import { FaBars } from "react-icons/fa";
+import Menu from "../../components/Menu";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-  
-  const [showMenu, setShowMenu] = useState(false)
-  
-  return(
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      screen.width > 900 ? setShowMenu(true) : setShowMenu(false);
+    });
+  }, []);
+
+  return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo_container}>
           <p>Logo </p>
         </div>
         <div className={styles.menu_container}>
-          <FaBars onClick={() => {setShowMenu(!showMenu)}} /> 
+          <FaBars
+            onClick={() => {
+              setShowMenu(!showMenu);
+            }}
+            className={styles.icon}
+          />
           <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
         </div>
       </div>
     </header>
-    )
-}
+  );
+};
 
-export default Header 
+export default Header;
