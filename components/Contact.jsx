a92cd2ba9cbc4b5136dc9a13faa13e18ce5c7e32
Nkematu Bonaventure <nkematu5@gmail.com>
2022-02-09 11:10:15 +0100

    updates

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,11 +1,17 @@
 import styles from "../styles/Contact.module.scss";
 import Form from "./Form";
+import Heading from "./Heading";
+import rocket from "../public/rocket.png";
 const Contact = () => {
   return (
     <section className={styles.contact}>
+      <Heading text={"Connect With Us"} />
       <div className={styles.container}>
-        <h3>Have A Request?</h3>
-        <h1>Get in touch with us</h1>
+        <div className="content">
+          <h3>Have A Request or Need A Website Design Consultations?</h3>
+          <h1>Get in touch with us and Let's Discuss on It.</h1>
+          <img src={rocket.src} alt="website development agency contact" />
+        </div>
         <Form />
       </div>
     </section>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,25 @@
 import styles from "../styles/Hero.module.scss";
 import Link from "next/link";
+import heroImg from "../public/herobg.png";
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
-        <h1>Need A Website?</h1>
-        <p>
-          Give your business and brand a wider reach with a unique website
-          tailored for your target audience
-        </p>
-        <div className={styles.btn_container}>
-          <Link className={styles.contact_link} href="#">
-            Get Started
-          </Link>
+        <div className={styles.hero_content}>
+          <h1>Need A Website?</h1>
+          <p>
+            Give your business and brand a wider reach with a unique website
+            tailored for your target audience
+          </p>
+          <div className={styles.btn_container}>
+            <Link className={styles.contact_link} href="#">
+              Get Started
+            </Link>
+          </div>
+        </div>
+        <div className={styles.hero_img}>
+          <img src={heroImg.src} alt="best web development agency" />
         </div>
       </div>
     </section>
