f90e101ec9a17a59fb1342f6ccc76bd5dd90d4ab
chulocr8v <nkematu5@gmail.com>
2022-06-12 22:46:40 +0100

    updates

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,25 +1,55 @@
 import styles from "../styles/Hero.module.scss";
 import Link from "next/link";
-import heroImg from "../public/herobg.png";
 import Image from 'next/image'
+import {FaGlobe, FaDraftingCompass, FaLaptopCode, FaInfo, FaSearchengin} from 'react-icons/fa'
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
+       
         <div className={styles.hero_content}>
-          <h1>Need A Website?</h1>
-          <p>
+          <h1 className={styles.hero_title} >Need A Website?</h1>
+          <p className={styles.hero_subtitle} >
             Give your business and brand a wider reach with a unique website
             tailored for your target audience
           </p>
-          <div className={styles.btn_container}>
-            <Link className={styles.contact_link} href="/contact">
-              Get Started
+       {/*   <div className={styles.btn_container}>
+            <Link className={styles.btn} href="/contact">
+              <a className={styles.btn}>
+                Get Started
+              </a>
             </Link>
-          </div>
-        </div>
-        <div className={styles.hero_img}>
-          <Image src="/herobg.png" height={200} width={400} alt="best web development agency in Nigeria" />
+          </div> */} 
+          <div className={styles.hero_services} >
+            <div className={styles.hero_service} >
+              <FaDraftingCompass className={styles.icon} />
+              <p className={styles.hero_service_title}>we DESIGN </p>
+            </div>
+            <div className={styles.hero_service} >
+              <FaLaptopCode className={styles.icon} />
+              <p className={styles.hero_service_title}>we DEVELOP </p>
+            </div>
+            <div className={styles.hero_service} >
+              <FaInfo className={styles.icon} />
+              <p className={styles.hero_service_title}>we SUPPORT</p>
+            </div>
+            <div className={styles.hero_service} >
+              <FaSearchengin className={styles.icon} />
+              <p className={styles.hero_service_title}>we OPTIMIZE</p>
+            </div>
+            </div>
+            <div className={styles.btn_wrapper}>
+              <Link href="/contact">
+                <a className={styles.btn}>
+                  Discuss Your Project
+                </a>
+              </Link>
+              <Link href="/projects">
+                <a className={styles.btn}>
+                  View Our Projects
+                </a>
+              </Link>
+            </div>
         </div>
       </div>
     </section>
